4e85d88b6f2423bb804fa63dfafda81a4f9f5d7b
JulienRousseau8 <jrousseau@excilys.com>
2020-08-27 09:56:20 +0200

    Add token

--- webapp/src/main/java/com/excilys/restController/ComputerRestController.java
+++ webapp/src/main/java/com/excilys/restController/ComputerRestController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping(value = "/api/computers")
@@ -71,8 +70,6 @@ public class ComputerRestController {
                 return ResponseEntity.ok(obj.writeValueAsString(computer.get().toString()));
             } catch (JsonProcessingException jsonExc) {
                 jsonExc.printStackTrace();
-
-
             }
         }
 
--- webapp/src/main/java/com/excilys/restController/LoginRestController.java
+++ webapp/src/main/java/com/excilys/restController/LoginRestController.java
@@ -6,15 +6,13 @@ import javax.naming.AuthenticationException;
 
 import com.excilys.dto.MyUserDTO;
 import com.excilys.service.UserService;
+import com.excilys.tokenJwt.JwtTokenUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -22,24 +20,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api")
 public class LoginRestController {
 
-    //private JwtTokenUtil jwtTokenUtil;
+    private JwtTokenUtil jwtTokenUtil;
     private final UserService userService;
     BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public LoginRestController(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
-        //this.jwtTokenUtil = jwtTokenUtil;
+    public LoginRestController(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder, JwtTokenUtil jwtTokenUtil) {
+        this.jwtTokenUtil = jwtTokenUtil;
         this.userService = userService;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
-//    @PostMapping(value = "/login")
-//    public ResponseEntity<?> login(@RequestBody MyUserDTO authentificationRequest) throws AuthenticationException {
-//
-//        authenticate(authentificationRequest.getUsername(), authentificationRequest.getPassword());
-//        final UserDetails userDetails = userService.loadUserByUsername(authentificationRequest.getUsername());
-//        final String token = jwtTokenUtil.generateToken(userDetails);
-//        return ResponseEntity.ok(token);
-//    }
+    @PostMapping(value = "/login")
+    public ResponseEntity<?> login(@RequestBody MyUserDTO authentificationRequest) throws AuthenticationException {
+
+        authenticate(authentificationRequest.getUsername(), authentificationRequest.getPassword());
+        final UserDetails userDetails = userService.loadUserByUsername(authentificationRequest.getUsername());
+        final String token = jwtTokenUtil.generateToken(userDetails);
+        return ResponseEntity.ok(token);
+    }
 
     @ExceptionHandler({AuthenticationException.class})
     public ResponseEntity<String> handleAuthenticationException(AuthenticationException e) {
